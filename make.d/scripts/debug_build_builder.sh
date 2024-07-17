bc43f0be33b9c594ff084f6e291e325d01e90a37
MetaBarj0 <troctsch.cpp@gmail.com>
2024-07-17 08:32:39 +0000

    refactor(make script): debug build builder
    refactor and fix

--- make.d/scripts/debug_build_builder.sh
+++ make.d/scripts/debug_build_builder.sh
@@ -1,35 +1,30 @@
 export BUILDX_EXPERIMENTAL=1
 
-# TODO: remove duplication
 if [ $USE_DOCKER_DESKTOP -eq 0 ]; then
-	docker buildx debug build \
-    --build-arg COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME} \
-    --build-arg NAMESPACE=${NAMESPACE} \
-    --build-arg REPOSITORY_URI=${REPOSITORY_URI} \
-    --build-arg REPOSITORY_REV=${REPOSITORY_REV} \
-    --build-arg REPOSITORY_SHA=${REPOSITORY_SHA} \
-    --build-arg USE_DOCKER_DESKTOP=${USE_DOCKER_DESKTOP} \
-    --build-arg USER=docker \
-    --build-arg USER_GID=$(getent group docker | cut -d : -f 3) \
-    --build-arg USER_HOME_DIR=/home/docker \
-    --build-arg USER_UID=$(id -u) \
-    -f docker.d/builder/builder.Dockerfile \
-    --ssh=default \
-    docker.d/builder
-else
-	docker buildx build \
-    --build-arg COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME} \
-    --build-arg NAMESPACE=${NAMESPACE} \
-    --build-arg REPOSITORY_URI=${REPOSITORY_URI} \
-    --build-arg REPOSITORY_REV=${REPOSITORY_REV} \
-    --build-arg REPOSITORY_SHA=${REPOSITORY_SHA} \
-    --build-arg USE_DOCKER_DESKTOP=${USE_DOCKER_DESKTOP} \
-    --build-arg USER=root \
-    --build-arg USER_GID=0 \
-    --build-arg USER_HOME_DIR=/root \
-    --build-arg USER_UID=0 \
-    --invoke /bin/bash \
-    -f docker.d/builder/builder.Dockerfile \
-    --ssh=default \
-    docker.d/builder
+    USER=docker
+    USER_GID=$(getent group docker | cut -d : -f 3)
+    USER_HOME_DIR=/home/docker
+    USER_UID=$(id -u)
 fi
+
+if [ $USE_DOCKER_DESKTOP -eq 0 ]; then
+    USER=root
+    USER_GID=0
+    USER_HOME_DIR=/root
+    USER_UID=0
+fi
+
+docker buildx debug build \
+  --build-arg COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME} \
+  --build-arg NAMESPACE=${NAMESPACE} \
+  --build-arg REPOSITORY_URI=${REPOSITORY_URI} \
+  --build-arg REPOSITORY_REV=${REPOSITORY_REV} \
+  --build-arg REPOSITORY_SHA=${REPOSITORY_SHA} \
+  --build-arg USER=${USER} \
+  --build-arg USER_GID=${USER_GID} \
+  --build-arg USER_HOME_DIR=${USER_HOME_DIR} \
+  --build-arg USER_UID=${USER_UID} \
+  -f docker.d/builder/builder.Dockerfile \
+  --ssh=default \
+  -t ${NAMESPACE}.builder:${BUILDER_VERSION} \
+  docker.d/builder
