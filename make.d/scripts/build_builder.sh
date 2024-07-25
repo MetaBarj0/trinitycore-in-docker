ea2d22a92d13da56ae9b5b7a46eaeddccdb7a4c7
MetaBarj0 <troctsch.cpp@gmail.com>
2024-07-25 10:10:36 +0200

    refactor: build_builder
    build script

--- make.d/scripts/build_builder.sh
+++ make.d/scripts/build_builder.sh
@@ -1,18 +1,29 @@
-# TODO: simplify, conditioning variables only
-if [ $USE_DOCKER_DESKTOP -eq 0 ]; then
-  docker compose -f docker.d/docker-compose.yml build \
-    --build-arg USER=docker \
-    --build-arg USER_GID=$(getent group docker | cut -d : -f 3) \
-    --build-arg USER_HOME_DIR=/home/docker \
-    --build-arg USER_UID=$(id -u) \
-    builder
-fi
+main() {
+  local user
+  local user_gid
+  local user_home_dir
+  local user_uid
 
-if [ $USE_DOCKER_DESKTOP -eq 1 ]; then
+  if [ $USE_DOCKER_DESKTOP -eq 0 ]; then
+    user=docker
+    user_gid=$(getent group docker | cut -d : -f 3)
+    user_home_dir=/home/docker
+    user_uid=$(id -u)
+  fi
+
+  if [ $USE_DOCKER_DESKTOP -eq 1 ]; then
+    user=root
+    user_gid=0
+    user_home_dir=/root
+    user_uid=0
+  fi
+
   docker compose -f docker.d/docker-compose.yml build \
-    --build-arg USER=root \
-    --build-arg USER_GID=0 \
-    --build-arg USER_HOME_DIR=/root \
-    --build-arg USER_UID=0 \
+    --build-arg USER=${user} \
+    --build-arg USER_GID=${user_gid} \
+    --build-arg USER_HOME_DIR=${user_home_dir} \
+    --build-arg USER_UID=${user_uid} \
     builder
-fi
+}
+
+main
