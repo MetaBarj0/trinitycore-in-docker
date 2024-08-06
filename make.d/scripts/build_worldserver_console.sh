fe9295fd171905f730949323c4ebabe389caa30f
MetaBarj0 <troctsch.cpp@gmail.com>
2024-08-06 15:40:26 +0200

    refactor(no_build_in_compose): worldserver_console
    build this service is not in the compose logic anymore

--- make.d/scripts/build_worldserver_console.sh
+++ make.d/scripts/build_worldserver_console.sh
@@ -5,6 +5,11 @@ archive_files_to \
   Makefile.maintainer.env \
   ./docker.d/worldserver_console/configuration_files.tar
 
-docker compose -f docker.d/docker-compose.yml build worldserver_console
+docker build \
+  --build-arg COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME} \
+  --build-arg NAMESPACE=${NAMESPACE} \
+  -t ${NAMESPACE}.worldserver_console:${WORLDSERVER_CONSOLE_VERSION} \
+  -f docker.d/worldserver_console/worldserver_console.Dockerfile \
+  docker.d/worldserver_console
 
 rm -f ./docker.d/worldserver_console/configuration_files.tar
